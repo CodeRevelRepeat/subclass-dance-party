f4ca1f0dc87b42529f3ce02c4808cab10cb1fe26
andyttran <andyttran@me.com>
2015-03-31 20:47:27 -0700

    Changed chicken and pig dance functionality

--- src/growDancer.js
+++ src/growDancer.js
@@ -1,9 +1,13 @@
 var makeGrowDancer = function(top, left, timeBetweenSteps){
-  makeDancer.call(this, top, left, timeBetweenSteps);
   this.width = 200;
   this.height = 200;
-  this.$node.addClass('chicken');
+  this.timestep = 1;
+  this.dx = 50;
+  this.dy = -50;
+  makeDancer.call(this, top, left, timeBetweenSteps);
 
+  this.$node.addClass('chicken');
+  
 };
 
 makeGrowDancer.prototype = Object.create(makeDancer.prototype);
@@ -14,8 +18,16 @@ makeGrowDancer.prototype.oldStep = makeDancer.prototype.step;
 
 makeGrowDancer.prototype.step = function(){
   this.oldStep();
-  this.$node.css({"width": this.width, "height": this.height});
-  this.width+= 10;
-  this.height+= 10;
+  if(!this.$node.hasClass("lineup")){
+    this.left = this.left + this.dx * Math.cos(this.timestep);
+    this.top = this.top + this.dy * Math.sin(this.timestep/10);
+    this.setPosition(this.top, this.left);
+    this.timestep++;
+    this.timeBetweenSteps -= 5;
+    if(this.timeBetweenSteps < 40){
+      this.$node.effect("explode");
+      this.$node.remove();
+    }
+  }
 };
 
